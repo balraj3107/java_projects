5bd2c69368e6e83c23482338cfb561185dfd5b3b
balraj3107 <88673817+balraj3107@users.noreply.github.com>
2021-12-22 18:22:08 +0530

    File read And write

--- untitled/src/Start.java
+++ untitled/src/Start.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 
 public class Start {
    public static void main(String args[]){
-       Scanner sc = new Scanner(System.in);
+
         Student student   = new Student();
-       System.out.println("Enter Batch Name : ");
-       String batch_name = sc.nextLine();
-        student.FileCreation(batch_name);
+
         student.init();
 
     }
--- untitled/src/Student.java
+++ untitled/src/Student.java
@@ -1,73 +1,82 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Student {
-   static final int TOTAL_STUDENTS=10;
-    static int currentIndex=-1;
-   static Student[] student = new Student[TOTAL_STUDENTS];
+    static final int TOTAL_STUDENTS = 10;
+    static int currentIndex = -1;
+    static Student[] student = new Student[TOTAL_STUDENTS];
     static Scanner sc = new Scanner(System.in);
     String name;
     String mobile;
-    String roll_number;
+    String rollNumber;
     String dob;
     File studentdata;
 
-    void FileCreation(String FilePath){
-        studentdata= new File(FilePath);
+    Student() {
+        studentdata = new File("STUDENTS");
         try {
-            if(studentdata.createNewFile()){
+            if (studentdata.createNewFile()) {
                 System.out.println("File is Created");
-            }
-            else{
-                System.out.println("File is already exists!!");
+            } else {
+                System.out.println(" ");
             }
         } catch (IOException e) {
             System.out.println("can't create File");
         }
     }
 
-    void start(){
+    void start() throws FileNotFoundException {
         int option;
 
-        System.out.println("press 1 to Add Students"+"\n"+"press 2 to print all Students");
+        System.out.println("press 1 to Add Students" + "\n" + "press 2 to print all Students");
         option = sc.nextInt();
-            switch (option) {
-                case 1 -> addStudent();
-                case 2 -> printStudents();
-                default -> System.out.println("not a valid option");
-            }
+        switch (option) {
+            case 1 -> addStudent();
+            case 2 -> printStudents();
+            default -> System.out.println("not a valid option");
         }
-    void init(){
-        int continueProcess=0;
-        do{
-            start();
-            System.out.print("press 1 to continue: ");
-            continueProcess= sc.nextInt();
-        }while(continueProcess==1);
     }
-        void addStudent(){
-        if(currentIndex>=TOTAL_STUDENTS-1){
+
+    void init() {
+        int continueProcess;
+
+        do {
+            try {
+                start();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.out.print("press 1 to continue: ");
+            continueProcess = sc.nextInt();
+        } while (continueProcess == 1);
+    }
+
+    void addStudent() {
+        if (currentIndex >= TOTAL_STUDENTS - 1) {
             System.out.println("Seats are full");
-        }else{
-            int StudentNumber=++currentIndex;
-            student[StudentNumber]= new Student();
+        } else {
+            int StudentNumber = ++currentIndex;
+            student[StudentNumber] = new Student();
             sc.nextLine();
             System.out.print("Enter Student Name: ");
-            student[StudentNumber].name = sc.nextLine();System.out.print("Enter Student mobile number: ");
-            student[StudentNumber].mobile = sc.nextLine();System.out.print("Enter Student roll number: ");
-            student[StudentNumber].roll_number = sc.nextLine();System.out.print("Enter Student dob: ");
+            student[StudentNumber].name = sc.nextLine();
+            System.out.print("Enter Student mobile number: ");
+            student[StudentNumber].mobile = sc.nextLine();
+            System.out.print("Enter Student roll number: ");
+            student[StudentNumber].rollNumber = sc.nextLine();
+            System.out.print("Enter Student dob: ");
             student[StudentNumber].dob = sc.nextLine();
-            String student_details = (student[StudentNumber].name+"|"+student[StudentNumber].mobile
-            +"|"+student[StudentNumber].roll_number+"|"+student[StudentNumber].dob+"\n" );
-            writeFile(student_details);
+            String studentDetails = (student[StudentNumber].name + "|" + student[StudentNumber].mobile
+                    + "|" + student[StudentNumber].rollNumber + "|" + student[StudentNumber].dob + "\n");
+            addStudentToFile(studentDetails);
         }
 
-        }
-    void writeFile(String data){
+    }
+
+    void addStudentToFile(String data) {
         try {
-            FileWriter fileWriter = new FileWriter(studentdata,true);
+            FileWriter fileWriter = new FileWriter(studentdata, true);
             //fileWriter.write(data);
             System.out.println(data);
             fileWriter.append(data);
@@ -77,16 +86,24 @@ public class Student {
         }
     }
 
-        void printStudents(){
-           for(int i = 0;i<currentIndex+1;i++){
-               System.out.print("name: "+student[i].name+" ");
-               System.out.print("mobile number : "+student[i].mobile+" ");
-               System.out.print("roll number : "+student[i].roll_number+" ");
-               System.out.print("dob : "+student[i].dob+" ");
-               System.out.println(" ");
-           }
-        }
 
+    void printStudents() throws FileNotFoundException {
+
+        Scanner sc = new Scanner(studentdata);    //file to be scanned
+        while (sc.hasNextLine()) {
+
+            String[] printStudentDetails;
+            String studentDetails = sc.nextLine();
+            printStudentDetails = studentDetails.split("\\|");
+
+            for(int i =0 ; i<printStudentDetails.length;i++)
+                System.out.print("  "+printStudentDetails[i]);
+            System.out.println("\n");
+
+
+        }
+        sc.close();
+    }
 
 
 }
