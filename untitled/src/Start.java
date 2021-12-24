ac7cb13d79f7ef80134d7f81c6fefe19024a3a8b
balraj3107 <88673817+balraj3107@users.noreply.github.com>
2021-12-24 19:29:07 +0530

    deleteFromArray

--- untitled/src/Start.java
+++ untitled/src/Start.java
@@ -1,11 +1,26 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Start {
    public static void main(String args[]){
+//
+//        Student student   = new Student();
+//
+//        student.init();
+       
+       int x =2;
+       deleteElement(x);
 
-        Student student   = new Student();
+    }
+    static int[] a = new int[]{2,6,5,1,7,4,3,2};
+      static void deleteElement(int index){
+          int n = a.length;
+        for(int i=index;i<(n-1);i++)
+            a[i]=a[i+1];
 
-        student.init();
+        n-=1;
+
+        System.out.println(Arrays.toString(a));
 
     }
 
