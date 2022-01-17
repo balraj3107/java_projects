625cacae61a73a163019f7bc635a5299c4795456
balraj3107 <88673817+balraj3107@users.noreply.github.com>
2022-01-17 19:16:15 +0530

    start

--- untitled/src/Start.java
+++ untitled/src/Start.java
@@ -1,13 +1,14 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Start {
-   public static void main(String args[]){
+   public static void main(String args[]) throws FileNotFoundException {
 //
-//        Student student   = new Student();
-//
-//        student.init();
-         Bike bike = new Bike();
-         bike.init();
+        Student student   = new Student();
+
+        student.init();
+//         Bike bike = new Bike();
+//         bike.init();
     }
 
 
